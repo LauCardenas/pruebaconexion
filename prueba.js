9f6da08327dc2ae90670108ac5c17705c3903cc1
LauCardenas <laura.lcardenas@mercadolibre.com.co>
2021-12-16 21:08:58 -0500

    newachives

--- prueba.js
+++ prueba.js
@@ -1,3 +1,13 @@
-//es una pruebita na mas porque no se si me seguira pidiendo 2auth
-// sjdjks
-console.log("ayuda 1 2 3")
+//Prueba JS
+function calculatePrice(nombre,precio){
+    let costEnvio
+    if (precio<=2000){costEnvio=300}
+    else if(2000<precio<=4000){costEnvio=500}
+    else{costEnvio=700}
+return `El producto ${nombre}cuesta ${precio}. 
+Su costo de envío es de ${costEnvio}. 
+Por lo tanto, el precio final es de ${costEnvio+precio}’`
+}
+console.log(calculatePrice('Macbook', 2500))
+console.log(calculatePrice('Celular', 500))
+console.log(calculatePrice('Playstation', 4500))
